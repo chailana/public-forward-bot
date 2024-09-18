52f33704a29f1d3c414404f633f36efe308968dc
chailana <chailana35217793@gmail.com>
2024-09-18 23:23:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -5,44 +5,42 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import asyncio
 import sys
 
-START_MSG="Hi {},\nThis is a simple bot to forward all messages from one channel to other\n\n⚠️Warning\nYour account may get banned if you forward more files(from private channels). Use at Own Risk!!"
-HELP_MSG="Available commands:-\n\n/index - To index a channel\n/forward - To start forwarding\n/total - Count total messages in DB\n/status - Check Current status\n/help - Help data\n/stop - To stop all running processes. \n\nUse /index to index messages from a channel to database.\n\nAfter indexing you can start forwarding by using /forward.\n\n<b>Note:</b>\nYou will require the following data to index a channel:-\n\n<b>Channel Invite Link</b>:- If channel is a Private channel User needs to join channel to acces the messages. Please note that do not leave channel until forwarding completes.\n\n<b>Channel ID</b>:- If channel is a private channel you may need to enter Channel ID. Get it from @MissRose_bot.\n\n<b>SKIP_NO</b>:-From where you want to start Forwarding files.Give 0 if from starting\n\n<b>Caption</b>:- Custom Caption for forwarded files. Use 0 to use default captions.\nAfter fowarding completes use the /cleardb command to clean your database."
-buttons=InlineKeyboardMarkup(
+START_MSG = "Hi {},\nThis is a simple bot to forward all messages from one channel to other\n\n⚠️Warning\nYour account may get banned if you forward more files(from private channels). Use at Own Risk!!"
+HELP_MSG = "Available commands:-\n\n/index - To index a channel\n/forward - To start forwarding\n/total - Count total messages in DB\n/status - Check Current status\n/help - Help data\n/stop - To stop all running processes. \n\nUse /index to index messages from a channel to database.\n\nAfter indexing you can start forwarding by using /forward.\n\n<b>Note:</b>\nYou will require the following data to index a channel:-\n\n<b>Channel Invite Link</b>:- If channel is a Private channel User needs to join channel to access the messages. Please note that do not leave channel until forwarding completes.\n\n<b>Channel ID</b>:- If channel is a private channel you may need to enter Channel ID. Get it from @MissRose_bot.\n\n<b>SKIP_NO</b>:-From where you want to start Forwarding files. Give 0 if from starting\n\n<b>Caption</b>:- Custom Caption for forwarded files. Use 0 to use default captions.\nAfter forwarding completes use the /cleardb command to clean your database."
+
+buttons = InlineKeyboardMarkup(
+    [
         [
-            [
-                InlineKeyboardButton("✨ Hᴇʟᴘ ✨", callback_data="help"),
-                InlineKeyboardButton("⚠️ Hᴏᴡ Dᴏᴇs I Wᴏʀᴋ? ⚠️", callback_data="abt")
-            ],
-            [
-                InlineKeyboardButton("🪄 Sᴏᴜʀᴄᴇ Cᴏᴅᴇ 🪄", url="https://t.me/CT_Arena/135"),
-                InlineKeyboardButton("💫 Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ 💫", url="https://t.me/CT_Arena")
-            ]
+            InlineKeyboardButton("✨ Hᴇʟᴘ ✨", callback_data="help"),
+            InlineKeyboardButton("⚠️ Hᴏᴡ Dᴏᴇs I Wᴏʀᴋ? ⚠️", callback_data="abt")
+        ],
+        [
+            InlineKeyboardButton("🪄 Sᴏᴜʀᴄᴇ Cᴏᴅᴇ 🪄", url="https://t.me/CT_Arena/135"),
+            InlineKeyboardButton("💫 Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ 💫", url="https://t.me/CT_Arena")
         ]
-        )
+    ]
+)
 
 @Client.on_message(filters.private & filters.command('start'))
 async def start(client, message):
     await client.send_message(
         chat_id=message.chat.id,
         text=START_MSG.format(
-                message.from_user.first_name),
+            message.from_user.first_name),
         reply_markup=buttons,
         parse_mode="markdown")
 
-
 @Client.on_message(filters.command("stop"))
 async def stop_button(bot, message):
-
     if str(message.from_user.id) not in Config.OWNER_ID:
         return
     msg = await bot.send_message(
-        text="Stoping all processes...",
+        text="Stopping all processes...",
         chat_id=message.chat.id
     )
     await asyncio.sleep(1)
     await msg.edit("All Processes Stopped and Restarted")
     os.execl(sys.executable, sys.executable, *sys.argv)
-
 
 @Client.on_message(filters.private & filters.command('help'))
 async def help(client, message):
@@ -51,12 +49,9 @@ async def help(client, message):
         text=HELP_MSG,
         parse_mode="markdown")
 
-
 @Client.on_callback_query(filters.regex(r'^help$'))
 async def cb_help(bot, cb):
     await cb.message.edit_text(HELP_MSG)
-
-
 
 @Client.on_callback_query(filters.regex(r'^abt$'))   
 async def cb_abt(bot, cb):
@@ -68,4 +63,4 @@ async def cb_abt(bot, cb):
             ]
         ]
     )
-    )
+                              )
